5c641b229428f0615b3b07ec45df00202a9b0250
andres <andres.jorquera@kairosds.com>
2019-08-14 12:15:17 +0200

    feat(setName): added tests

--- functions/licenceAppointments/utils.js
+++ functions/licenceAppointments/utils.js
@@ -1,5 +1,10 @@
 module.exports = {
     setOfficeName: name => {
-        return name.toLowerCase().replace(' ', '-');
+        return name
+            .toLowerCase()
+            // replace any accents 
+            .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            // replace any spaces "/" "." ". "
+            .replace(/(\. |-|\.| |\/)/g, '-');
     }
 };
